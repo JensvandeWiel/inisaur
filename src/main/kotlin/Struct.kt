fcf8578bd628c54f32951273809a9f59df8679cd
Jens van de Wiel <jens.vdwiel@gmail.com>
2025-05-23 16:31:04 +0200

    Made struct values being a IniValue optional on adding

--- src/main/kotlin/Struct.kt
+++ src/main/kotlin/Struct.kt
@@ -12,8 +12,24 @@ fun Struct.toIniValue(): IniValue {
     return IniValue(this, IniValueType.Struct)
 }
 
-fun structOf(vararg pairs: Pair<String, IniValue>): Struct {
-    return mapOf(*pairs)
+fun structOf(vararg pairs: Pair<String, Any?>): Struct {
+    val map = mutableMapOf<String, Any?>()
+    for (pair in pairs) {
+        val key = pair.first
+        val value = pair.second
+        map[key] = when (value) {
+            is Map<*, *> -> (value as Struct).toIniValue()
+            is String -> IniValue(value)
+            is Boolean -> IniValue(value)
+            is Int -> IniValue(value)
+            is Float -> IniValue(value)
+            null -> IniValue(null as String?)
+            is IniValue -> value
+            else -> throw IllegalArgumentException("Invalid type for struct value: ${value::class.java}")
+        }
+    }
+
+    return map
 }
 
 fun structOfAsIniValue(vararg pairs: Pair<String, IniValue>): IniValue {
