5c83d37fef9ffa1c0b60dab509adb463f2890a2d
Jens van de Wiel <jens.vdwiel@gmail.com>
2025-05-25 21:06:16 +0200

    Added release and test workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import java.net.URI
 plugins {
     kotlin("jvm") version "2.1.20"
     id("org.jetbrains.dokka") version "2.0.0"
+    id("maven-publish")
 }
 
 group = "eu.wynq"
@@ -40,4 +41,69 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+val dokkaHtml by tasks.getting(DokkaTask::class)
+
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn(dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(dokkaHtml.outputDirectory)
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
+
+            pom {
+                name.set("Inisaur")
+                description.set("A Kotlin library for parsing INI files")
+                url.set("https://github.com/JensvandeWiel/inisaur")
+
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("JensvandeWiel")
+                        name.set("Jens van de Wiel")
+                        email.set("jens@wynq.eu")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/JensvandeWiel/inisaur.git")
+                    developerConnection.set("scm:git:ssh://github.com/JensvandeWiel/inisaur.git")
+                    url.set("https://github.com/JensvandeWiel/inisaur")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
 }
